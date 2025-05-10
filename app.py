23355f372eb4c929399f211affd5fecc01467edb
electronpower <khalidqacememip@gmail.com>
2025-05-10 22:14:20 +0200

    Updating the api message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     if os.getenv("ENVIRONMENT") == "production":
-        return {"message": "You have mastered all the topics efficiently - Updated with GitHub Actions!"}
+        return {"message": "You have mastered all the topics efficiently with azure container instances - Updated with GitHub Actions!"}
     else:
         return {"message": "Yes you have mastered the concept - Local Version"}
     
