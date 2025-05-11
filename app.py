31f7d135c7d14aa7f9abe4b417fe5b220bb17820
electronpower <khalidqacememip@gmail.com>
2025-05-11 12:53:10 +0200

    Updating the app message to test both the app and the github Actions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     if os.getenv("ENVIRONMENT") == "production":
-        return {"message": "You have mastered all the topics efficiently with azure app service - Updated with GitHub Actions!"}
+        return {"message": "You have mastered all the topics efficiently with azure app service instead of just a container instance - Updated with GitHub Actions!"}
     else:
         return {"message": "Yes you have mastered the concept - Local Version"}
     
