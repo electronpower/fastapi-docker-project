d6d243a1751dd4d4712a3d0071bbd354a588edbc
electronpower <khalidqacememip@gmail.com>
2025-05-11 12:43:12 +0200

    Adapting the deploy file so it can work for a web app insteade of a container instance, udpated the app message

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     if os.getenv("ENVIRONMENT") == "production":
-        return {"message": "You have mastered all the topics efficiently with azure container instances - Updated with GitHub Actions!"}
+        return {"message": "You have mastered all the topics efficiently with azure app service - Updated with GitHub Actions!"}
     else:
         return {"message": "Yes you have mastered the concept - Local Version"}
     
 @app.get("/version")
 def get_version():
-    return {"version": "1.0.0"} 
+    return {"version": "1.0.1"} 
